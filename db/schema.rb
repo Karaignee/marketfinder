f7a217a43d71bf8cf194c92a23e21f5703322910
Karaignee <karen_rooney@hotmail.com>
2013-03-23 16:50:09 +0000

    Added description attribute to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130313195646) do
+ActiveRecord::Schema.define(:version => 20130323162611) do
 
   create_table "events", :force => true do |t|
     t.string   "title"
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(:version => 20130313195646) do
     t.string   "location"
     t.float    "longitude"
     t.float    "latitude"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.text     "description"
   end
 
 end
