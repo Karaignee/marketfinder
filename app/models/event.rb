6d9e38f84edbd07dcc9769beb729095ee63a20a7
Karaignee <karen_rooney@hotmail.com>
2013-03-23 16:51:11 +0000

    Allow event description to be editable

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :end, :latitude, :location, :longitude, :repeat, :start, :title
+  attr_accessible :end, :latitude, :location, :longitude, :repeat, :start, :title, :description
   geocoded_by :location
   after_validation :geocode, :if => :location_changed?
 
