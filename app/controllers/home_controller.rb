028cde1e2f73c24f607922d4d9f23aa31efb6e8a
Karaignee <karen_rooney@hotmail.com>
2013-04-14 13:59:12 +0100

    Added upcoming events to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,9 @@ class HomeController < ApplicationController
     @eventJSON = Event.all.to_json
     
     @today = Time.now.beginning_of_day()
-    @todays_events = Event.where("start <= ? AND end <= ?", @today, @today)
+    @todays_events = Event.where("start <= ? AND end >= ?", @today, @today)
+    
+    @upcoming = Event.where("start > ?", @today)
     
     respond_to do |format|
       format.html
