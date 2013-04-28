b7b2f97279ef3e5d73194b88040e59406f087870
Karaignee <karen_rooney@hotmail.com>
2013-04-28 15:33:38 +0100

    Added server side validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,16 @@ class Event < ActiveRecord::Base
   geocoded_by :location
   after_validation :geocode, :if => :location_changed?
   
+  validates :title, :presence => true
+  
+  validates :start, :format => {:with => /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/((19|20)[0-9]{2})$/i}
+  
+  validates :end, :format => {:with => /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[012])\/((19|20)[0-9]{2})$/i}
+  
+  validates :location, :presence => true
+  
+  validates :description, :presence => true
+  
   # simple_calander requires a field called "start_time" but it's called "start" in our database
   # this function will map "start_time" to our real "start" field
   def start_time
